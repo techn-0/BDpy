a1de6bcb82ceaec6c472827642c6db9a20eecd4a
Alisser <gnlrjswjd@naver.com>
2024-06-09 22:47:48 +0900

    f

--- 텍스트 빈도분석.py
+++ 텍스트 빈도분석.py
@@ -62,3 +62,61 @@ for title in all_title:
 
 
     #데이터 탐색
+
+    count = Counter(words2)
+
+count   #출력하여 내용 확인
+
+word_count = dict()
+
+for tag, counts in count.most_common(50):
+    if(len(str(tag))>1):
+        word_count[tag] = counts
+        print("%s : %d" % (tag, counts))
+
+
+
+del word_count['big']
+del word_count['data']
+
+#히스토그램
+plt.figure(figsize=(12,5))
+plt.xlabel("word")
+plt.ylabel("count")
+plt.grid(True)
+
+sorted_Keys = sorted(word_count, key=word_count.get, reverse=True)
+sorted_Values = sorted(word_count.values(), reverse=True)
+
+plt.bar(range(len(word_count)), sorted_Values, align='center')
+plt.xticks(range(len(word_count)), list(sorted_Keys), rotation='85')
+
+plt.show()
+
+#그래프 그리기
+#연도별데이터
+all_files_data_concat['doc_count'] = 0
+summary_year = all_files_data_concat.groupby('출판일', as_index=False)['doc_count'].count()
+summary_year  #출력하여 내용 확인
+
+
+
+plt.figure(figsize=(12,5))
+plt.xlabel("year")
+plt.ylabel("doc-count")
+plt.grid(True)
+
+plt.plot(range(len(summary_year)), summary_year['doc_count'])
+plt.xticks(range(len(summary_year)), [text for text in summary_year['출판일']])
+
+plt.show()
+
+#워드 클라우드
+stopwords=set(STOPWORDS)
+wc=WordCloud(background_color='ivory', stopwords=stopwords, width=800, height=600)
+cloud=wc.generate_from_frequencies(word_count)
+
+plt.figure(figsize=(8,8))
+plt.imshow(cloud)
+plt.axis('off')
+plt.show()
